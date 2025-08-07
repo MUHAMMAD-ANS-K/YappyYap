79b78c7cf2dfa92f3647c76e9d899d349b963ff0
MUHAMMAD-ANS-K <ansmuhammad340@gmail.com>
2025-08-08 02:36:34 +0500

    Added a new pkg cross-env for cross envs

--- Frontend/YappyYap/vite.config.js
+++ Frontend/YappyYap/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base : process.env.NODE_ENV === 'production' ? "/YappyYap/": '/',
+  base : process.env.DEPLOY_FROM === 'GIT' ? "/YappyYap/": '/',
   plugins: [react()],
 })
