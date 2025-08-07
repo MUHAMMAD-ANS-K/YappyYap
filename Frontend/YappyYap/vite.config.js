525ec7877b700305491a9bdb886e58c156c2e90e
MUHAMMAD-ANS-K <ansmuhammad340@gmail.com>
2025-08-08 02:55:10 +0500

    Test for hosting on vercel

--- Frontend/YappyYap/vite.config.js
+++ Frontend/YappyYap/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const test = process.env.DEPLOY_FROM
+console.log(test)
 export default defineConfig({
   base : process.env.DEPLOY_FROM === 'GIT' ? "/YappyYap/": '/',
   plugins: [react()],
