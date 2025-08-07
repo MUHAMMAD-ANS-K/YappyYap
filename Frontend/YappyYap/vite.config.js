11f060260c9515b40d87a7288159ad831ae2f5ab
MUHAMMAD-ANS-K <ansmuhammad340@gmail.com>
2025-08-08 03:18:27 +0500

    again hosting error

--- Frontend/YappyYap/vite.config.js
+++ Frontend/YappyYap/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 const test = process.env.DEPLOY_FROM
 console.log(test)
 export default defineConfig({
-  base : process.env.DEPLOY_FROM === 'GIT' ? "/YappyYap/": '/',
+  base : process.env.DEPLOY_FROM === 'VRC' ? "/": '/YappyYap/',
   plugins: [react()],
 })
