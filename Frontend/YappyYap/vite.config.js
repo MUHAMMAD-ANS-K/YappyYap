3ad58c3c9f2a4983ccec7d480c275cb2bb584702
MUHAMMAD-ANS-K <ansmuhammad340@gmail.com>
2025-08-08 02:21:05 +0500

    Again configured vite.config.js for vercel deployment

--- Frontend/YappyYap/vite.config.js
+++ Frontend/YappyYap/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base : "/YappyYap/",
+  base : process.env.NODE_ENV === 'production' ? "/YappyYap/": '/',
   plugins: [react()],
 })
