7e5ba5039f2a31a1b6899294959b7850b25b607c
MUHAMMAD-ANS-K <ansmuhammad340@gmail.com>
2025-08-10 16:40:27 +0500

    Changed the main app for railway deployment

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 import auth
+import os
 from fastapi.middleware.cors import CORSMiddleware
-
+from uvicorn import run
+port = int(os.environ.get("PORT", 8000))
 app = FastAPI()
 app.include_router(auth.router)
 
@@ -16,3 +18,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__=="__main__":
+    run("main:app", host="0.0.0.0", port=port)
