bc71920cedf57ba1848d7262935e35b351575a7c
MUHAMMAD-ANS-K <ansmuhammad340@gmail.com>
2025-08-17 19:43:11 +0500

    Again a change for railway deployment of port flexibilty

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import auth, websocket
+import os
+import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(websocket.router)
@@ -39,3 +42,6 @@ users = []
 #             if user in users:
 #                 users.remove(user)
 
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
